a22c213b78249b7f2a5ce35085c716ebc5441734
sergiogon <sergiogon@microsoft.com>
2024-04-30 14:24:17 +0200

    Improved comments

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# prompt_scorer
+# Prompt Scorer
+
+This repository contains the `prompt_scorer` module, a Python-based module for scoring system prompts against a set of prompt engineering best practices.
+
+
+## Usage
+
+The `example.py` script uses this module
--- example.py
+++ example.py
@@ -1,9 +1,13 @@
 
 from openai import AzureOpenAI
+from dotenv import load_dotenv
 import os
 
 import prompt_scorer
 
+load_dotenv()
+
+# Load environment variables
 AZURE_OPENAI_ENDPOINT=os.environ["AZURE_OPENAI_ENDPOINT"]
 AZURE_OPENAI_API_KEY=os.environ["AZURE_OPENAI_API_KEY"]
 AZURE_OPENAI_MODEL_DEPLOYMENT_NAME=os.environ["AZURE_OPENAI_MODEL_DEPLOYMENT_NAME"]
@@ -14,10 +18,10 @@ llm_client = AzureOpenAI(
   azure_endpoint = AZURE_OPENAI_ENDPOINT, 
   api_key = AZURE_OPENAI_API_KEY,  
   api_version = AZURE_OPENAI_API_VERSION
-
 )
 
-
+# Sample input prompt
 input_prompt = f"""The description for this product should be fairly short, a few sentences only, and not too much more."""
 
+# Score the prompt calling the scoring function
 prompt_scorer.score_prompt(input_prompt, llm_client, AZURE_OPENAI_MODEL_DEPLOYMENT_NAME)
